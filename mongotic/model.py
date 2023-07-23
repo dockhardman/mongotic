13bc6d73535bfbfa43c868a7c95e1ce4fecf59bd
Allen Chou <f1470891079@gmail.com>
2023-07-23 16:02:53 +0800

    MongoBaseModel with __databasename__ attrs.

--- mongotic/model.py
+++ mongotic/model.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
 
+NOT_SET_SENTINEL = object()
+
 
 class MongoBaseModel(BaseModel):
-    pass
+    __tablename__ = NOT_SET_SENTINEL
+    __databasename__ = NOT_SET_SENTINEL
+
+
+if __name__ == "__main__":
+    model = MongoBaseModel()
+    print(model.model_dump())
