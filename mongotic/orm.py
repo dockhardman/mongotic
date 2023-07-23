a276998da03bfb614372c666bcd600f4f1a16787
Allen Chou <f1470891079@gmail.com>
2023-07-23 18:50:25 +0800

    Add function sessionmaker to manager pymongo client session.

--- mongotic/orm.py
+++ mongotic/orm.py
@@ -1,26 +1,54 @@
-from typing import Any
+from typing import Any, Protocol, Type
 
 from pymongo import MongoClient
+from typing_extensions import ParamSpec
+
+P = ParamSpec("P")
 
 
-def sessionmaker():
-    def __init__(self, bind_engine: "MongoClient", **kwargs: Any):
-        self.engine = bind_engine
+class Session(Protocol):
+    engine: "MongoClient"
+
+    def __init__(self, bind_engine: MongoClient, **kwargs: Any):
+        ...
 
     def query(self, *args: Any, **kwargs: Any):
-        return
+        ...
 
     def add(self, *args: Any, **kwargs: Any):
-        return
+        ...
 
-    def commit(self, *args: Any, **kwargs: Any):
-        return
+    def commit(self, *args: Any, **kwargs: Any) -> None:
+        ...
 
-    # use type to create a new Session class
-    Session = type(
-        "Session",
-        (),
-        {"__init__": __init__, "query": query, "add": add, "commit": commit},
-    )
+    def __enter__(self):
+        ...
 
-    return Session
+    def __exit__(self, exc_type, exc_value, traceback):
+        ...
+
+
+def sessionmaker() -> Type[Session]:
+    class _Session:
+        def __init__(self, bind_engine: "MongoClient", **kwargs: Any):
+            self.engine = bind_engine
+
+        def query(self, *args: Any, **kwargs: Any):
+            return
+
+        def add(self, *args: Any, **kwargs: Any):
+            return
+
+        def commit(self, *args: Any, **kwargs: Any) -> None:
+            with self.engine.start_session() as session:
+                pass
+
+        def __enter__(self):
+            self.session = self.engine.start_session()
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.session.end_session()
+            self.session = None
+
+    return _Session
