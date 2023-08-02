e3f3153d0072ac5919db976778605e2f5a8fb2e3
Allen Chou <f1470891079@gmail.com>
2023-08-02 22:42:57 +0800

    Test model filed operators.

--- tests/test_model_filed_operators.py
+++ tests/test_model_filed_operators.py
@@ -9,7 +9,10 @@ from pymongo import MongoClient
 from mongotic.model import MongoBaseModel
 from mongotic.orm import sessionmaker
 
+test_name = f"test_{rand_str(10)}"
+test_email = f"test_{rand_str(10)}@example.com"
 test_company = f"test_{rand_str(10)}"
+test_age = 25
 
 
 class User(MongoBaseModel):
@@ -29,13 +32,116 @@ def test_init_documents(mongo_engine: "MongoClient"):
     session = Session()
 
     new_user = User(
-        name="Alice Moe",
-        email="alice.moe@example.com",
+        name=test_name,
+        email=test_email,
         company=test_company,
-        age=25,
+        age=test_age,
     )
     session.add(new_user)
     session.commit()
+
+
+def test_query_filter_operators(mongo_engine: "MongoClient"):
+    Session = sessionmaker(bind=mongo_engine)
+    session = Session()
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name == test_name)
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name == "NAME NOT EXISTS")
+        .all()
+    )
+    assert len(users) == 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name != test_name)
+        .all()
+    )
+    assert len(users) == 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name != "NAME NOT EXISTS")
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.age > test_age)
+        .all()
+    )
+    assert len(users) == 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.age > test_age - 1)
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.age >= test_age)
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.age < test_age)
+        .all()
+    )
+    assert len(users) == 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.age < test_age + 1)
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.age <= test_age)
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name.in_([test_name]))
+        .all()
+    )
+    assert len(users) > 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name.in_(["NAME NOT EXISTS"]))
+        .all()
+    )
+    assert len(users) == 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name.not_in([test_name]))
+        .all()
+    )
+    assert len(users) == 0
+
+    users = (
+        session.query(User)
+        .filter(User.company == test_company, User.name.not_in(["NAME NOT EXISTS"]))
+        .all()
+    )
+    assert len(users) > 0
 
 
 def test_clean_documents(mongo_engine: "MongoClient"):
